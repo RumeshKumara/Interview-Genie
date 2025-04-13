9bdaf8b29ba16f2757b01998289f53466e457353
rumesh <rumeshk066@gmail.com>
2025-04-13 15:19:39 +0530

    add userButton

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 function Dashboard() {
-  return <div>dashboard</div>;
+  return (
+    <div>
+      dashboard
+      <UserButton />
+    </div>
+  );
 }
 
 export default Dashboard;
