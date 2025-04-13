e62da719510f4fc9d5418e71838905763155b479
rumesh <rumeshk066@gmail.com>
2025-04-13 22:33:16 +0530

    fix some error

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,10 +1,15 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, use } from "react";
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 import { BsMoon, BsSun } from "react-icons/bs";
+import { usePathname } from "next/navigation";
 
 function Header() {
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path);
+  }, []);
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
@@ -23,22 +28,32 @@ function Header() {
     <div className="flex justify-between items-center p-4 bg-transparent dark:bg-[#121212]">
       <div className="flex items-center gap-2">
         <Image src="/logo.svg" alt="Logo" width={40} height={50} />
-        <h2 className="text-2xl font-bold text-[#5417d7] dark:text-[#5417d7]">
+        <h2 className=" hidden md:flex text-2xl font-bold text-[#5417d7] dark:text-[#5417d7]">
           InterviewGenie
         </h2>
       </div>
       <div className="p-[2px] bg-gradient-to-r from-[#5417d7] via-[#8b5cf6] to-[#5417d7] rounded-full animate-gradient bg-[length:200%_200%]">
-        <ul className="flex gap-8 bg-white dark:bg-[#1e1e1e] backdrop-blur-sm p-4 px-8 rounded-full text-[#5f22d1] dark:text-[#e2e2e2] font-semibold items-center">
-          <li className="hover:text-[#8b5cf6] transition-colors cursor-pointer">
+        <ul className=" hidden md:flex gap-8 bg-white dark:bg-[#1e1e1e] backdrop-blur-sm p-2 px-2 pr-4 rounded-full text-[#444444] dark:text-[#e2e2e2] font-semibold items-center">
+          <li
+            className={`hover:text-[#8b5cf6] transition-colors cursor-pointer
+                ${path == "/dashboard" && " bg-[#d8cdff] dark:bg-[#2a2149] px-[18px] py-2 text-[#5100ff] dark:text-[#7433ff] rounded-full"}
+                `}
+          >
             Dashboard
           </li>
-          <li className="hover:text-[#8b5cf6] transition-colors cursor-pointer">
+          <li
+            className={`hover:text-[#8b5cf6] transition-colors cursor-pointer`}
+          >
             Questions
           </li>
-          <li className="hover:text-[#8b5cf6] transition-colors cursor-pointer">
+          <li
+            className={`hover:text-[#8b5cf6] transition-colors cursor-pointer`}
+          >
             Upgrade
           </li>
-          <li className="hover:text-[#8b5cf6] transition-colors cursor-pointer">
+          <li
+            className={`hover:text-[#8b5cf6] transition-colors cursor-pointer`}
+          >
             How it Works?
           </li>
         </ul>
