8279092a3318252403cd89e4a1ff642925d3e29b
rumesh <rumeshk066@gmail.com>
2025-05-27 13:29:50 +0530

    fix button click after go to dashboard

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
+"use client";
+
 import { Button } from "../components/ui/button";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+import { FaArrowRight, FaGithub } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-white ">
       <Image
@@ -16,7 +23,10 @@ export default function Home() {
       </p>
       <div className="flex flex-col items-center gap-4">
         <div className="flex gap-2 ">
-          <button className="mt-4 bg-[#5417d7] hover:bg-[#5417d7d2] transition-all duration-300 ease-in-out  rounded-full px-6 py-2 text-white text-lg flex items-center gap-2">
+          <button
+            className="mt-4 bg-[#5417d7] hover:bg-[#5417d7d2] transition-all duration-300 ease-in-out  rounded-full px-6 py-2 text-white text-lg flex items-center gap-2"
+            onClick={() => router.push("/dashboard")}
+          >
             Get Start
             <FaArrowRight size={18} />
           </button>
@@ -25,11 +35,11 @@ export default function Home() {
           </button>
         </div>
         <div className="flex gap-2">
-          <button className="flex items-center gap-2 px-6 py-2 text-lg text-black transition-all duration-300 ease-in-out bg-white border-gray-500 rounded-full  hover:bg-black hover:text-white border-1">
+          <button className="flex items-center gap-2 px-6 py-2 text-lg text-black transition-all duration-300 ease-in-out bg-white border-gray-500 rounded-full hover:bg-black hover:text-white border-1">
             <FaGithub size={24} /> {/* GitHub icon */}
             GitHub Resource
           </button>
-          <button className="flex items-center gap-2 px-6 py-2 text-lg text-black transition-all duration-300 ease-in-out bg-white border-gray-500 rounded-full  hover:bg-black hover:text-white border-1">
+          <button className="flex items-center gap-2 px-6 py-2 text-lg text-black transition-all duration-300 ease-in-out bg-white border-gray-500 rounded-full hover:bg-black hover:text-white border-1">
             How it works?
           </button>
         </div>
